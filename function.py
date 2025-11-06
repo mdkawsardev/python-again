494f42495c3fe762c43a33984bb648d29ce952b8
mdkawsar <info.mdkawsardev@gmail.com>
2025-11-06 23:56:11 +0600

    dflkd

--- function.py
+++ function.py
@@ -7,3 +7,11 @@ print((a + b + c)/3) # Average number of these 3 numbers
 def greet():
     return("Good morning")
 print(greet())
+
+# factorial of input number
+def factorial(n):
+    if(n==1 or n==0):
+        return 1
+    return n * factorial(n-1)
+n = int(input("Enter a number: "))
+print(factorial(n))
