99879c28b450341445bb1555e9e2b5d03ebc9c67
mdkawsar <info.mdkawsardev@gmail.com>
2025-11-06 23:34:06 +0600

    kdf

---
+++ function.py
@@ -0,0 +1,5 @@
+a = 23
+b = 50
+c = 44
+
+print((a + b + c)/3) # Average number of these 3 numbers
--- problem1.py
+++ problem1.py
@@ -3,3 +3,5 @@ for i in range(1, n+1):
     print(" " * (n-i), end="")
     print("*" * (2*i-1))
 
+for i in range(1, 11):
+    print(f"{n} X {11 - i} = {n*(11 -i)}")
