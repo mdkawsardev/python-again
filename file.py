0ecd8f7f10697d1ce82c4d2403b511983d10a30d
mdkawsar <info.mdkawsardev@gmail.com>
2025-11-07 21:01:16 +0600

    dkfjd

--- file.py
+++ file.py
@@ -1,11 +1,15 @@
 # Reading a file
-file = open("text.txt", "r")
-read = file.read()
-print(read)
-file.close()
+# file = open("text.txt", "r")
+# read = file.read()
+# print(read)
+# file.close()
 
-# Writing a file
-description = "Hello sir, You have been hired to our company"
-f = open("message.txt", "w")
-f.write(description)
-f.close()
+# # Writing a file
+# description = "Hello sir, You have been hired to our company"
+# f = open("message.txt", "w")
+# f.write(description)
+# f.close()
+
+# Reading files using with statement, and no need to close files
+with open("message.txt", "r") as op:
+    print(op.read())
