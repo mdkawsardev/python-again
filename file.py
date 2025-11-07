94ac140b318437f5c31ea760aceb040c8fdf9b42
mdkawsar <info.mdkawsardev@gmail.com>
2025-11-07 20:48:06 +0600

    dkfd

--- file.py
+++ file.py
@@ -1,4 +1,11 @@
-file = open("text.txt")
+# Reading a file
+file = open("text.txt", "r")
 read = file.read()
 print(read)
 file.close()
+
+# Writing a file
+description = "Hello sir, You have been hired to our company"
+f = open("message.txt", "w")
+f.write(description)
+f.close()
