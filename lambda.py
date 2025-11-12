da43377525175684940b8905b9823c9ea2d953e1
mdkawsar <info.mdkawsardev@gmail.com>
2025-11-12 21:27:09 +0600

    dkfld

--- lambda.py
+++ lambda.py
@@ -4,7 +4,7 @@ def myfunc(x):
 print(myfunc(5))
 
 # This is same as above function. Lambda is a short way to write in a sinlge line of code
-func = lambda x: x*x
+func = lambda x: x*x # lambda a an anonymous function
 print(func(5))
 
 l = ["kawsar", "imran", "emon", "asif"]
